b0ea0042a2d7223baaafc0cb5780e6656c6e5d6d
Jean Borges <jeanborgesdares@gmail.com>
2025-02-05 16:34:48 -0300

    Setup Tailwind CSS and initial layout

---
+++ src/pages/about/About.tsx
@@ -0,0 +1,11 @@
+
+function About() {
+    return (
+        <div className="p-10 text-center">
+            <h1 className="text-3xl font-bold text-blue-600">Sobre Mim</h1>
+            <p className="mt-4 text-gray-700">Aqui você encontrará mais informações sobre meu trabalho.</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+
+function Home() {
+    return (
+        <div className="text-center">
+            <h1 className="text-4xl font-bold text-blue-600">Bem-vindo ao Meu Portfólio!</h1>
+            <p className="mt-4 text-red ">Esta é a página inicial.</p>
+        </div>
+    );
+}
+
+export default Home;
